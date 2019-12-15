6799d26ff1aa3264108f24343947f86078e14a02
KwanIkJang <44356083+rhksdlr134@users.noreply.github.com>
2019-12-15 15:26:59 +0900

    191215 15:26 minicolors 테스트해보셈

--- salab/src/main/webapp/resources/js/editPrivateFile/dragndrop.js
+++ salab/src/main/webapp/resources/js/editPrivateFile/dragndrop.js
@@ -688,7 +688,7 @@ $(document).on('mousedown', '#droppable', function(){
 	        }
 		});
 	}
-	/*$('.colorView').minicolors('settings', {
+	$('.colorView').minicolors('settings', {
         control: 'hue',
         position : "bottom right",
         defaultValue: "#FFFFFF",
@@ -702,7 +702,7 @@ $(document).on('mousedown', '#droppable', function(){
             	case "textground" : applyChange("textgroundColor"); break;
             }
         }
-    });*/
+    });
     minicolorsAddMenu($('#canvas-background'));
     $('.colorView').each(function(){
     	minicolorsAddMenu($(this).parent('.minicolors'));
